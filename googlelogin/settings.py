b251d3d59b1fd395499c158cb15265c7f1cab960
sahad sathar <sahadsathar1997@gmail.com>
2022-12-17 21:51:43 +0500

    first commit

--- googlelogin/settings.py
+++ googlelogin/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d(44feuabira6e@4v@+^v!obo_wml6!_*--#mnogx7#qdi88r0'
+SECRET_KEY = 'YOUR sECRET kEYS'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
